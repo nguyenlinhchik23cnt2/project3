148fe041cc8c87bc17c8dfc2e1be810dd8c7bf83
Linh Chi <nguyenlinhchi022@gmail.com>
2025-11-10 21:38:38 +0700

    lad03

---
+++ demo/src/main/java/k23cnt2/nlc/lesson03/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package k23cnt2.nlc.lesson03.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/k23cnt2/nlc/lesson03/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package k23cnt2.nlc.lesson03.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
